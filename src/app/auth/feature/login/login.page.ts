ac47d12bcf24a5aa43fe9fcb196bdef45cd0f18f
peggylicious <p.asq@live.com>
2023-04-19 21:47:35 +0100

    #2 Add login page with routing

--- src/app/auth/feature/login/login.page.ts
+++ src/app/auth/feature/login/login.page.ts
@@ -2,19 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { AuthFormComponent } from '../../ui/auth-form/auth-form.component';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, AuthFormComponent]
 })
 export class LoginPage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+  loginUser(event:any){
+
   }
 
 }
