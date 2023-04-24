e86cadbd8f00a01b301c03717a1c540a8d720c58
peggylicious <p.asq@live.com>
2023-04-24 13:28:58 +0100

    #18
    Setup past and upcoming events sections for event-list page

--- src/app/events/feature/event-list/event-list.page.ts
+++ src/app/events/feature/event-list/event-list.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { EventsService } from '../../data-access/events.service';
+import { EventForm } from 'src/app/models/event.interface';
 
 @Component({
   selector: 'app-event-list',
@@ -12,13 +13,22 @@ import { EventsService } from '../../data-access/events.service';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class EventListPage implements OnInit {
-  allEvents:[] = [];
+  allEvents: EventForm[] = [];
+  upComingEvents: EventForm[] = [];
+  pastEvents: EventForm[] = [];
   constructor(private events: EventsService) { }
 
   ngOnInit() {
     this.events.getEvents().subscribe(res=>{
       console.log(res)
       this.allEvents = res
+      this.allEvents.forEach(event => {
+        if(new Date(event.time).getTime()  < new Date().getTime()){
+          this.pastEvents.push(event)
+        }else{
+          this.upComingEvents.push(event)
+        }
+      })
     })
   }
 
