b8af87ec3f701aa640cad9d1777d8ff064bbf48a
peggylicious <p.asq@live.com>
2023-05-01 05:45:39 +0100

    #42 fix text display issue on andriod. Remove unwanted headers. Fix toggling of logout button

--- src/app/auth/data-access/auth.service.ts
+++ src/app/auth/data-access/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { loggedInUser, User } from 'src/app/models/auth.interface';
 
 @Injectable({
@@ -10,7 +11,15 @@ export class AuthService {
   // baseUrl = "http://localhost:3000/user"
   baseUrl = "https://birthday-reminder-server-rah7.onrender.com/user"
   // isLoggedIn: boolean = false;
+  isLoggedOut: BehaviorSubject<boolean> = new BehaviorSubject(false)
+  isLoggedOut$ = this.isLoggedOut.asObservable()
   constructor( private http: HttpClient, private router: Router) { }
+  get currentValueOfStateObj(): any {
+    return this.isLoggedOut.getValue();
+  }
+  private set logger(val: boolean){
+    this.isLoggedOut.next(val)
+  }
   register(data: User){
     console.log(data)
     return this.http.post(`${this.baseUrl}/signup`, data)
@@ -18,19 +27,27 @@ export class AuthService {
 
   login(data: User){
     console.log(data)
+    this.logger = true
     return this.http.post<loggedInUser>(`${this.baseUrl}/login`, data)
   }
   isLoggedIn(){
     let token = localStorage.getItem('reminderToken')
     console.log(token)
     if(!token){
+    // this.isLoggedOut.next(true)
+    this.logger=true
       return false
     }
+    // this.isLoggedOut.next(true)
+    this.logger=true
+
     return true
   }
   logOut(){
     localStorage.removeItem("access_token")
     console.log(localStorage.getItem("access_token"))
-    this.router.navigate(['/home'])
+    // this.isLoggedOut.next(false)
+    this.logger=false
+    this.router.navigateByUrl('/home',{state: {showBtn: false}})
   }
 }
