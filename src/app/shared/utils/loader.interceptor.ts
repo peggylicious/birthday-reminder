847863f291cc184c093ae2fe8e7d11a76bbb1814
peggylicious <p.asq@live.com>
2023-05-03 22:53:07 +0100

    #47 Catch Auth Errors and show loader when more than one async request on page

--- src/app/shared/utils/loader.interceptor.ts
+++ src/app/shared/utils/loader.interceptor.ts
@@ -10,11 +10,19 @@ import { LoaderService } from '../data-access/loader.service';
 
 @Injectable()
 export class LoaderInterceptor implements HttpInterceptor {
-
+  private totalRequests = 0;
   constructor(private loaderService: LoaderService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    console.log('caught '+ request.url)
+
+    this.totalRequests++;
     this.loaderService.show()
-    return next.handle(request).pipe(finalize(() => this.loaderService.hide()))
+    return next.handle(request).pipe(finalize(() => {
+      this.totalRequests--;
+      if (this.totalRequests == 0) {
+        this.loaderService.hide()
+      }
+    }))
   }
 }
